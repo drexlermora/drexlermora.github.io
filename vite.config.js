00d0fdb317429bf8a3713c7bc35dd3894f7173b4
john.mora <john.mora@sanyutechnos-ph.com>
2024-11-29 16:49:27 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/drexlermora.github.io/',  // Set this to the GitHub repo name (case-sensitive)
+  base: '/',  // No subdirectory, deploy to the root of the GitHub Pages domain
 });
