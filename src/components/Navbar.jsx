1ffb0e4aef4872cc2d591988455924b3417111a2
drexlermora <morajohndrexler@gmail.com>
2024-11-29 22:34:09 +0800

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
+      <div className="logo">
+      </div>
       <ul className={`nav-list ${isMobile ? "mobile" : ""}`}>
         <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
         <li><Link to="about" smooth={true} duration={500}>About</Link></li>
