1437bb60625d18d710dbd49d4c84fcc98581818b
drexlermora <morajohndrexler@gmail.com>
2024-11-29 22:28:04 +0800

    update

--- src/components/others/CardComponent.jsx
+++ src/components/others/CardComponent.jsx
@@ -4,7 +4,6 @@ import { Card, Button } from 'react-bootstrap';
 const CardComponent = ({ title, text, imageSrc, link }) => {
   return (
     <Card style={{ height: '21rem' }}>
-     
       <Card.Body>
         <Card.Title>{title} <Card.Img variant="top" src={imageSrc} alt="Card image" /></Card.Title>
         <Card.Text>{text}</Card.Text>
